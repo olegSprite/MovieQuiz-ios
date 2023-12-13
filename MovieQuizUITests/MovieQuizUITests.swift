107f5211db80ac318dc9f9decd403c2e91959e7f
Oleg Spiridonov <spiri300@yandex.ru>
2023-12-13 17:21:43 +0500

    -

--- MovieQuizUITests/MovieQuizUITests.swift
+++ MovieQuizUITests/MovieQuizUITests.swift
@@ -6,36 +6,92 @@
 //
 
 import XCTest
+@testable import MovieQuiz
 
-final class MovieQuizUITests: XCTestCase {
-
+class MovieQuizUITests: XCTestCase {
+    // swiftlint:disable:next implicitly_unwrapped_optional
+    var app: XCUIApplication!
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
+        try super.setUpWithError()
+        
+        app = XCUIApplication()
         app.launch()
+        
+        // это специальная настройка для тестов: если один тест не прошёл,
+        // то следующие тесты запускаться не будут; и правда, зачем ждать?
+        continueAfterFailure = false
+    }
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+        
+        app.terminate()
+        app = nil
+    }
+    
+    func testYesButton() {
+        sleep(3)
+        let firstPoster = app.images["Poster"] // находим первоначальный постер
+        let firstPosterData = firstPoster.screenshot().pngRepresentation
+        
+        app.buttons["Yes"].tap() // находим кнопку `Да` и нажимаем её
+        sleep(3)
+        
+        let secondPoster = app.images["Poster"] // ещё раз находим постер
+        let secondPosterData = secondPoster.screenshot().pngRepresentation
+        
+        let indexLabel = app.staticTexts["Index"]
+           
+        XCTAssertEqual(indexLabel.label, "2/10")
+        XCTAssertFalse(firstPosterData == secondPosterData)  // проверяем, что постеры разные
+    }
+    
+    func testNoButton() {
+        sleep(3)
+        let firstPoster = app.images["Poster"] // находим первоначальный постер
+        let firstPosterData = firstPoster.screenshot().pngRepresentation
+        
+        app.buttons["No"].tap() // находим кнопку `Нет` и нажимаем её
+        sleep(3)
+        
+        let secondPoster = app.images["Poster"] // ещё раз находим постер
+        let secondPosterData = secondPoster.screenshot().pngRepresentation
+        
+        let indexLabel = app.staticTexts["Index"]
+           
+        XCTAssertEqual(indexLabel.label, "2/10")
+        XCTAssertFalse(firstPosterData == secondPosterData)  // проверяем, что постеры разные
+    }
+    
+    func testGameFinish() {
+        sleep(3)
+        for _ in 1...10 {
+            app.buttons["No"].tap()
+            sleep(3)
+        }
 
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let alert = app.alerts.firstMatch
+        
+        XCTAssertTrue(alert.exists)
+        XCTAssertTrue(alert.label == "Этот раунд окончен!")
+        XCTAssertTrue(alert.buttons.firstMatch.label == "Сыграть еще раз")
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
+    func testAlertDismiss() {
+        sleep(3)
+        for _ in 1...10 {
+            app.buttons["No"].tap()
+            sleep(3)
         }
+        
+        let alert = app.alerts.firstMatch
+        alert.buttons.firstMatch.tap()
+        
+        sleep(3)
+        
+        let indexLabel = app.staticTexts["Index"]
+        
+        XCTAssertFalse(alert.exists)
+        XCTAssertTrue(indexLabel.label == "1/10")
     }
 }
