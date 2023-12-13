1d9f9254fbd2ce9237765d54564cb85c752ffc75
Oleg Spiridonov <spiri300@yandex.ru>
2023-12-13 17:21:21 +0500

    Добавлены разные тесты из спринта

---
+++ MovieQuizTests/ArrayTests.swift
@@ -0,0 +1,8 @@
+//
+//  ArrayTests.swift
+//  MovieQuizTests
+//
+//  Created by Олег Спиридонов on 10.12.2023.
+//
+
+import Foundation
---
+++ MovieQuizTests/MoviesLoaderTests.swift
@@ -0,0 +1,8 @@
+//
+//  MoviesLoaderTests.swift
+//  MovieQuizTests
+//
+//  Created by Олег Спиридонов on 10.12.2023.
+//
+
+import Foundation
